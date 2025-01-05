2bb7f9f3d38218c2fb88643ae382762465231401
Allenbrd <51126429+Allenbrd@users.noreply.github.com>
2025-01-05 16:21:29 +0200

    sign-up page

--- src/app/sign-up/[[...rest]]/page.tsx
+++ src/app/sign-up/[[...rest]]/page.tsx
@@ -1,13 +1,10 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function LoginPage() {
   return (
     <main className="h-screen w-full flex items-center justify-center bg-gray-50">
       <div className="w-full max-w-md p-8">
-        <h1 className="text-2xl font-bold text-center mb-8 text-gray-800">
-          Welcome Back
-        </h1>
-        <SignIn />
+        <SignUp />
       </div>
     </main>
   );
