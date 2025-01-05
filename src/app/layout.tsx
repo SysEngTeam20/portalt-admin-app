0cc7f19664769bd2f9661595215dce6e4a594651
Allenbrd <51126429+Allenbrd@users.noreply.github.com>
2025-01-05 16:29:09 +0200

    Update layout.tsx
    Fixed layout font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,21 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 import './globals.css'
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={inter.className}>
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
