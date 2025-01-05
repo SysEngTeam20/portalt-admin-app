f4d4ed7e95f35bfa059a5650e6d09d412d4b4c12
Allenbrd <51126429+Allenbrd@users.noreply.github.com>
2025-01-05 16:19:58 +0200

    sign-in page + app layout
    Created sign-in page composed of a welcome title and the Clerk <SignIn /> component.
    Modified app's layout to include ClerkProvider for auth context.

---
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -0,0 +1,16 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function LoginPage() {
+  return (
+    <main className="h-screen w-full flex items-center justify-center bg-gray-50">
+      <div className="w-full max-w-md p-8">
+        <h1 className="text-2xl font-bold text-center mb-8 text-gray-800">
+          Welcome Back
+        </h1>
+        <SignIn
+          // fallbackRedirectUrl="/organization-select"
+        />
+      </div>
+    </main>
+  );
+}
