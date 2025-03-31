b55831878d14672d9e4cefd5cee2d25cde05347e
Allen <allen.brd.75@gmail.com>
2025-03-31 23:38:13 +0100

    favicons

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,17 @@ const nextConfig: NextConfig = {
     bodyParser: {
       sizeLimit: '50mb'
     }
-  }
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ico|png|jpg|jpeg|gif|svg)$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
